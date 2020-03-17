995eb0fa952e9e482f8a4f72dbde9c211da51356
jakubklimek97 <jakukli853@student.polsl.pl>
2020-03-17 19:44:49 +0100

    Added SceneManager and first take on making code more logical

---
+++ src/main/java/pl/polsl/gk/tabletopSimulator/Game.java
@@ -0,0 +1,75 @@
+package pl.polsl.gk.tabletopSimulator;
+
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+import pl.polsl.gk.tabletopSimulator.Scenes.SceneList;
+import pl.polsl.gk.tabletopSimulator.Scenes.SceneLoading;
+import pl.polsl.gk.tabletopSimulator.Scenes.SceneManager;
+
+import java.nio.IntBuffer;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+public class Game {
+    private long window;
+
+    public void run() {
+        init();
+        main();
+
+        glfwFreeCallbacks(window);
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+        glfwSetErrorCallback(null).free();
+    }
+
+    private void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+        if ( !glfwInit() )
+            throw new IllegalStateException("ERROR::Failed to initialize GLFW");
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+        window = glfwCreateWindow(1280, 720, "Tabletop Simulator", NULL, NULL);
+        if ( window == NULL )
+            throw new RuntimeException("ERROR::Failed to create the GLFW window");
+
+        try ( MemoryStack stack = stackPush() ) {
+            IntBuffer pWidth = stack.mallocInt(1);
+            IntBuffer pHeight = stack.mallocInt(1);
+
+            glfwGetWindowSize(window, pWidth, pHeight);
+
+            GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+
+            glfwSetWindowPos(
+                    window,
+                    (vidmode.width() - pWidth.get(0)) / 2,
+                    (vidmode.height() - pHeight.get(0)) / 2
+            );
+        }
+
+        glfwMakeContextCurrent(window);
+        //Włącz VSync
+        glfwSwapInterval(1);
+
+        glfwShowWindow(window);
+    }
+
+    private void main() {
+        GL.createCapabilities();
+        SceneManager sceneManager = new SceneManager(window);
+        sceneManager.SwitchScene(SceneList.LOADING);
+    }
+
+    public static void main(String[] args) {
+        new Game().run();
+    }
+}

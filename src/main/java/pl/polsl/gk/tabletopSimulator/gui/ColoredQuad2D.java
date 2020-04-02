a031ae768eb7ab2b929e4653424d9df3b9030c28
jakubklimek97 <jakukli853@student.polsl.pl>
2020-04-02 19:10:10 +0200

    202: Testing git stash and moving between branches

---
+++ src/main/java/pl/polsl/gk/tabletopSimulator/gui/ColoredQuad2D.java
@@ -0,0 +1,26 @@
+package pl.polsl.gk.tabletopSimulator.gui;
+
+import pl.polsl.gk.tabletopSimulator.Math.Vector.Vector4f;
+import pl.polsl.gk.tabletopSimulator.utility.Shader;
+
+public class ColoredQuad2D extends GuiElement{
+    public ColoredQuad2D(float posX, float posY, float width, float height, Vector4f color){
+        super(posX, posY, width, height);
+        this.color = color;
+    }
+    public void AssignColorId(float r, float g, float b){
+
+    }
+    public float[] GetColorId(){
+        return new float[4];
+    }
+    public void Render(Shader shader){
+
+    }
+    float[] vertices;
+    int vao;
+    int vbo;
+    float posX, posY, width, height;
+    Vector4f color;
+
+}

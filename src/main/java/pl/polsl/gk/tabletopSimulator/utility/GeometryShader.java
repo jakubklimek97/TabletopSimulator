e2ddc4b99b87e395dad2df5802b39c3a1a4b7c25
jakubklimek97 <jakukli853@student.polsl.pl>
2020-04-21 01:34:23 +0200

    10103: Added shader utilizing geometry shaders

--- src/main/java/pl/polsl/gk/tabletopSimulator/utility/GeometryShader.java
+++ src/main/java/pl/polsl/gk/tabletopSimulator/utility/GeometryShader.java
@@ -1,29 +1,34 @@
 package pl.polsl.gk.tabletopSimulator.utility;
+
 import org.lwjgl.system.MemoryStack;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.IntBuffer;
+import java.util.HashMap;
 
 import static org.lwjgl.opengl.GL33C.*;
+import static org.lwjgl.opengl.GL20C.glDeleteShader;
 import static org.lwjgl.system.MemoryStack.stackPush;
 
-public class Shader {
-    protected  Map<String, Integer> uniforms = null;
-    protected int shaderId;
-    protected boolean isGood;
-    public Shader(){}
-    public Shader(String name){
+public class GeometryShader extends Shader {
+
+    public GeometryShader(String name){
+        uniforms = new HashMap<>();
         this.isGood = false;
         String vertexCode = new String();
         String fragmentCode = new String();
+        String geometryCode = new String();
         InputStream vertexUrl = getClass().getClassLoader().getResourceAsStream("shaders/"+name+".vs");
         InputStream fragmentUrl = getClass().getClassLoader().getResourceAsStream("shaders/"+name+".fs");
+        InputStream geometryUrl = getClass().getClassLoader().getResourceAsStream("shaders/"+name+".gs");
         if(!(vertexUrl != null && fragmentUrl != null)){
             System.err.println("ERROR::Couldn't load shader " + name);
         }
         try {
             vertexCode = ParseFileToString(vertexUrl);
             fragmentCode = ParseFileToString(fragmentUrl);
+            geometryCode = ParseFileToString(geometryUrl);
         } catch (IOException e) {
             //TODO
             // Exception when can't parse files
@@ -31,34 +36,49 @@ public class Shader {
         }
         int vertexShader = glCreateShader(GL_VERTEX_SHADER);
         int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        int geometryShader = glCreateShader(GL_GEOMETRY_SHADER);
         glShaderSource(vertexShader, vertexCode);
         glShaderSource(fragmentShader, fragmentCode);
+        glShaderSource(geometryShader, geometryCode);
         glCompileShader(vertexShader);
         glCompileShader(fragmentShader);
-        int vertexCompileStatus, fragmentCompileStatus;
+        glCompileShader(geometryShader);
+        int vertexCompileStatus, fragmentCompileStatus, geometryCompileStatus;
         try(MemoryStack stack = stackPush()){
             IntBuffer ip = stack.callocInt(1);
             glGetShaderiv(vertexShader, GL_COMPILE_STATUS, ip);
             vertexCompileStatus = ip.get(0);
             glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, ip);
             fragmentCompileStatus = ip.get(0);
+            glGetShaderiv(geometryShader, GL_COMPILE_STATUS, ip);
+            geometryCompileStatus = ip.get(0);
         }
         if(fragmentCompileStatus != 1){
             System.out.println("ERROR::FRAGMENTSHADER::"+name);
             System.out.println(glGetShaderInfoLog(fragmentShader));
             glDeleteShader(vertexShader);
             glDeleteShader(fragmentShader);
+            glDeleteShader(geometryShader);
         }
         else if(vertexCompileStatus != 1){
             System.out.println("ERROR::VERTEXSHADER::"+name);
             System.out.println(glGetShaderInfoLog(fragmentShader));
             glDeleteShader(vertexShader);
             glDeleteShader(fragmentShader);
+            glDeleteShader(geometryShader);
+        }
+        else if(geometryCompileStatus != 1){
+            System.out.println("ERROR::GEOMETRYSHADER::"+name);
+            System.out.println(glGetShaderInfoLog(geometryShader));
+            glDeleteShader(vertexShader);
+            glDeleteShader(fragmentShader);
+            glDeleteShader(geometryShader);
         }
         else{
             shaderId = glCreateProgram();
             glAttachShader(shaderId, vertexShader);
             glAttachShader(shaderId, fragmentShader);
+            glAttachShader(shaderId, geometryShader);
             glLinkProgram(shaderId);
             int shaderCompileStatus;
             try(MemoryStack stack = stackPush()){
@@ -77,30 +97,8 @@ public class Shader {
             }
             glDeleteShader(vertexShader);
             glDeleteShader(fragmentShader);
+            glDeleteShader(geometryShader);
         }
 
-    }
-    protected String ParseFileToString(InputStream file) throws IOException {
-        StringBuilder strB = new StringBuilder();
-        try(      BufferedReader br = new BufferedReader(new InputStreamReader(file));){
-            String tmp;
-            while((tmp = br.readLine()) != null) {
-                strB.append(tmp).append("\n");
-            }
-        }
-        return strB.toString();
-    }
-    public boolean Use(){
-        if(!isGood){
-            return false;
-        }
-        glUseProgram(shaderId);
-        return true;
-    }
-    private int shaderId;
-    private boolean isGood;
-
-    public boolean isGood() {
-        return isGood;
     }
 }
